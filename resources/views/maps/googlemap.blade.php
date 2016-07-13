175ea3e5fd489a1871f859e02872656f4852872a
takooctopus <takooctopus@hotmail.com>
2016-07-13 16:22:15 +0800

    fix oauth

--- resources/views/maps/googlemap.blade.php
+++ resources/views/maps/googlemap.blade.php
@@ -20,16 +20,67 @@
 <script>
 
     var map;
+    var marker;
     function initMap() {
-        var tianjinuniversity = new google.maps.LatLng(39.107918850334265, 117.1730089187622);
-        var map = new google.maps.Map(document.getElementById('map'), {
-                center: tianjinuniversity,
-                zoom: 15
-            });
+        var tjuOld = new google.maps.LatLng(39.107918850334265, 117.1730089187622);
+        var mapOptions = {
+            center: tjuOld,
+            zoom: 15
+        };
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        marker = new google.maps.Marker({
+            position : tjuOld,
+            map: map,
+            draggable: false,
+            //animation: google.maps.Animation.DROP,
+            title: 'Hello World!'
+        });
+        marker.addListener('click', toggleBounce);
+
+        //信息窗口
+        var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">天津大学</h1>'+
+                '<div id="bodyContent">'+
+                '<p><b>天津大学</b>, also referred to as <b>Tianjin University</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    }
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
     }
 
+
+
+
+
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYjVhYKfncsuJqUiR80_vKPL6WxaMY_tE&callback=initMap"
+<script src="http://maps.google.cn/maps/api/js?key=AIzaSyDYjVhYKfncsuJqUiR80_vKPL6WxaMY_tE&callback=initMap"
         async defer></script>
 </body>
 </html>
